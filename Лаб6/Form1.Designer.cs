ee56d2fc1350f1223671b5b93568550af12fd4e9
Lisuko Лисуко <liza.shevchuk08@gmail.com>
2025-05-10 15:02:08 +0300

    Add project files.

---
+++ Лаб6/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Лаб6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConeBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PyramidBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ConeBox1.SuspendLayout();
+            this.PyramidBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ConeBox1
+            // 
+            this.ConeBox1.Controls.Add(this.label5);
+            this.ConeBox1.Controls.Add(this.textBox5);
+            this.ConeBox1.Controls.Add(this.label2);
+            this.ConeBox1.Controls.Add(this.label1);
+            this.ConeBox1.Controls.Add(this.textBox2);
+            this.ConeBox1.Controls.Add(this.textBox1);
+            this.ConeBox1.Controls.Add(this.button1);
+            this.ConeBox1.Location = new System.Drawing.Point(5, 3);
+            this.ConeBox1.Name = "ConeBox1";
+            this.ConeBox1.Size = new System.Drawing.Size(238, 159);
+            this.ConeBox1.TabIndex = 0;
+            this.ConeBox1.TabStop = false;
+            this.ConeBox1.Text = "Конус";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 14);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "V";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(49, 84);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "H";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "обрахунок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PyramidBox2
+            // 
+            this.PyramidBox2.Controls.Add(this.label6);
+            this.PyramidBox2.Controls.Add(this.textBox6);
+            this.PyramidBox2.Controls.Add(this.label3);
+            this.PyramidBox2.Controls.Add(this.label4);
+            this.PyramidBox2.Controls.Add(this.textBox3);
+            this.PyramidBox2.Controls.Add(this.textBox4);
+            this.PyramidBox2.Controls.Add(this.button2);
+            this.PyramidBox2.Location = new System.Drawing.Point(5, 169);
+            this.PyramidBox2.Name = "PyramidBox2";
+            this.PyramidBox2.Size = new System.Drawing.Size(238, 158);
+            this.PyramidBox2.TabIndex = 1;
+            this.PyramidBox2.TabStop = false;
+            this.PyramidBox2.Text = "Піраміда";
+            this.PyramidBox2.Enter += new System.EventHandler(this.ConeBox2_Enter);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 14);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "V";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(49, 84);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "H";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 14);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "A";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(49, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(49, 30);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "обрахунок";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 344);
+            this.Controls.Add(this.PyramidBox2);
+            this.Controls.Add(this.ConeBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "лабораторна 6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ConeBox1.ResumeLayout(false);
+            this.ConeBox1.PerformLayout();
+            this.PyramidBox2.ResumeLayout(false);
+            this.PyramidBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox ConeBox1;
+        private System.Windows.Forms.GroupBox PyramidBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Лаб6/Solid.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб6
+{
+    internal abstract class Solid
+    {
+        public abstract double GetVolume();
+    }
+    internal class Pyramid: Solid
+    {
+        private double a;
+        private double h;
+        public double A
+        {
+            get { return a; }
+            set 
+            {
+                if (value >= 0)
+                {
+                    a = value;
+                }
+            }
+        }
+        public double H
+        {
+            get { return h; }
+            set
+            {
+                if (value >= 0)
+                {
+                    h = value;
+                }
+            }
+        }
+        public override double GetVolume() 
+        {
+            return 1.0/3 * a * a * h;
+        }
+    }
+     internal class Cone: Solid
+    {
+        private double r;
+        private double h;
+
+        public double R
+        {
+            get { return r; }
+            set
+            {
+                if (value >= 0)
+                {
+                    r = value;
+                }
+            }
+        }
+        public double H
+        {
+            get { return h; }
+            set
+            {
+                if (value >= 0)
+                {
+                    h = value;
+                }
+            }
+        }
+
+        public override double GetVolume()
+        {
+            return 1.0/3 * r * r * h;
+        }
+    }
+}
